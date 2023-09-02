245ec94344b37e017521da9e3912ec3fba6a3e0a
krishna kumar <kri821084@gmail.com>
2023-09-02 14:57:05 +0530

    added sove hover na dtransform scalte and Top nav in page

--- src/Pages/Help/Main.jsx
+++ src/Pages/Help/Main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Styles from "./Styles.module.css"
+import { Top_nav } from '../../Components'
 function Main() {
   return (
     <div className={Styles.Main_div}>
-  
+    <Top_nav Page_name={"Help"}/>
 
     </div>
   )
--- src/Pages/Setting/Main.jsx
+++ src/Pages/Setting/Main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Styles from "./Styles.module.css"
+import { Top_nav } from '../../Components'
 function Main() {
   return (
     <div className={Styles.Main_div}>
   
-
+  <Top_nav Page_name={"Setting"}/>
     </div>
   )
 }
