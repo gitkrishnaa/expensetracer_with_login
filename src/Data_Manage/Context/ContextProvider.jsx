c69a2ecbfb7c26b613c6c44e733709217be7b2ae
krishna kumar <kri821084@gmail.com>
2023-09-06 18:58:00 +0530

    created a function in data managemeny context which convert data in to year and month wise in object

--- src/Data_Manage/Context/ContextProvider.jsx
+++ src/Data_Manage/Context/ContextProvider.jsx
@@ -1,38 +1,73 @@
-import React, { useContext, useState } from 'react'
-import ContextCreate from "./ContextCreate"
-import { Monthly_payContextCreate } from '../../Context'
-import { ExpenseContextCreate } from '../../Context'
-import { BudgetContextCreate } from '../../Context'
-import fakeData from "../data"
-import { useReducer } from 'react'
+import React, { useContext, useState } from "react";
+import ContextCreate from "./ContextCreate";
+import { Monthly_payContextCreate } from "../../Context";
+import { ExpenseContextCreate } from "../../Context";
+import { BudgetContextCreate } from "../../Context";
+import fakeData from "../data";
+import { useReducer } from "react";
 function Provider(props) {
+  const Expense = useContext(ExpenseContextCreate);
+  const budget = useContext(BudgetContextCreate);
+  const Monthly_Pay = useContext(Monthly_payContextCreate);
+  const check = () => {
+    console.log(new Date(fakeData[207].Expense_Date).getMonth());
+    console.log(new Date(fakeData[207].Expense_Date));
 
-const Expense=useContext(ExpenseContextCreate);
-const budget=useContext(BudgetContextCreate);
-const Monthly_Pay=useContext(Monthly_payContextCreate);
-const check=()=>{
-alert("check data management")
-console.log(Expense.data)
-console.log(budget.data)
-console.log(Monthly_Pay.data)
-console.log(fakeData)
-}
- 
-// fake data in expense
-const feed_expense_data=()=>{
-  Expense.largeFake_data(fakeData)
-  console.log(Expense,budget,Monthly_Pay)
-}
+    // const xyz=new Date('2023-8-2')
+   console.log(change_Data_format_to_year_and_month_wise())
+  };
 
+  // fake data in expense
+  const feed_expense_data = () => {
+    Expense.largeFake_data(fakeData);
+    console.log(Expense, budget, Monthly_Pay);
+  };
 
+  function change_Data_format_to_year_and_month_wise  ()  {
+    const dataArr=Expense.data;
+    const newArrData_format = {};
+    for (let i = 0; i < dataArr.length; i++) {
+      const data = dataArr[i];
+      const month = new Date(data.Expense_Date).getMonth() + 1;
+      const year = new Date(data.Expense_Date).getFullYear();
+      if (newArrData_format[`${year}`] == undefined) {
+        newArrData_format[`${year}`] = {};
+      }
+      if (newArrData_format[`${year}`][`${month}`] == undefined) {
+        newArrData_format[`${year}`][`${month}`] = [];
+      }
+      newArrData_format[`${year}`][`${month}`].push(data);
+    }
+console.log("this is function which convert expense context data convert to {year:{month:[{},{},{}]} function from-data manage context")
+    console.log(newArrData_format)
+    console.log("this function return data {year:{month:[{},{},{}]}");
+return newArrData_format;
+  };
+  const convert_Data_format_to_year_and_month_wise_data_as_argument = (dataArr) => {
+    const newArrData_format = {};
+    for (let i = 0; i < dataArr.length; i++) {
+      const data = dataArr[i];
+      const month = new Date(data.Expense_Date).getMonth() + 1;
+      const year = new Date(data.Expense_Date).getFullYear();
+      if (newArrData_format[`${year}`] == undefined) {
+        newArrData_format[`${year}`] = {};
+      }
+      if (newArrData_format[`${year}`][`${month}`] == undefined) {
+        newArrData_format[`${year}`][`${month}`] = [];
+      }
+      newArrData_format[`${year}`][`${month}`].push(data);
+    }
+    console.log("this is function which convert expense context data convert to {year:{month:[{},{},{}]} function from-data manage context")
+    console.log(newArrData_format)
+    console.log("this function take data as argument[{},{}] return data {year:{month:[{},{},{}]}");
+    return newArrData_format;
+  };
 
   return (
-    <ContextCreate.Provider value={{check,feed_expense_data}}>
-
-{props.children}
-
+    <ContextCreate.Provider value={{ check, feed_expense_data }}>
+      {props.children}
     </ContextCreate.Provider>
-  )
+  );
 }
 
-export default Provider
+export default Provider;
--- src/ExpensePageComponent/Transaction/Main.jsx
+++ src/ExpensePageComponent/Transaction/Main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Styles from "./Styles.module.css";
 import { Transaction_list_render } from '..';
 import { ExpenseContextCreate } from '../../Context';
@@ -6,12 +6,39 @@ import { useContext } from 'react';
 
 function Main() {
   const ExpenseContext=useContext(ExpenseContextCreate);
+const MainData=ExpenseContext.data;
+// const [arr,setArr]=useState(0);
 
+// const newrenderDataArray=[];
+// let j=0;
+// for(let i=(arr*20);i<((arr+1)*20);i++){
+//   newrenderDataArray[j]=MainData[i];
+//   j++;
+// }
+
+// const paginate_increase=()=>{
+//   // if((MainData.length/20)<(arr+2)){
+//   //   alert("last page, it cant increase more");
+//   //   return;
+//   // }
+ 
+//   setArr(a=>a+1)
+//   console.log(arr)
+//   console.log(newrenderDataArray)
+// }
+// const paginate_decrease=()=>{
+//   if(arr==0){
+//     alert("its zero, it cant decrease more");
+//     return
+//   }
+//   setArr(a=>a-1)
+//   console.log(arr)
+//   console.log(newrenderDataArray)
+// }
   return (
     <div className={Styles.Main_div}>
-      <button>load More</button>
-  {ExpenseContext.data.map((e,n)=>{
-  return n<20?<Transaction_list_render data={e}/>:console.log()})}
+  {MainData.map((e,n)=>{
+  return <Transaction_list_render data={e}/>})}
 
     </div>
   )
